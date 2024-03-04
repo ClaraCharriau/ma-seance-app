5beb17e976d72838d7a002fb29510eafd752f470
Clara Charriau <charriau.clara@gmail.com>
2024-03-04 12:06:54 +0100

    bugfix: dont use date format to call GET /screenings

--- src/component/common/week-navigation/WeekNavigation.tsx
+++ src/component/common/week-navigation/WeekNavigation.tsx
@@ -1,12 +1,10 @@
 import style from './WeekNavigation.module.css';
 import useWeekDays, { IDay } from '../../../hooks/dates/useWeekDays';
 import { NavLink, useParams } from 'react-router-dom';
-import { addDays, formatISO } from 'date-fns';
 
 const WeekNavigation = () => {
     const { theaterId } = useParams();
     const weekDays: IDay[] = useWeekDays();
-    const today = new Date();
 
     const navLinkClassName = (isActive: boolean, isPending: boolean) =>
         isPending ? `${style.dayButtonPending}` : isActive ? `${style.dayButtonActive}` : `${style.dayButton}`;
@@ -16,7 +14,7 @@ const WeekNavigation = () => {
             <nav className={style.plannerWrapper}>
                 {weekDays.map((day, index) => (
                     <NavLink
-                        to={`/theaters/${theaterId}/${formatISO(addDays(today, index), { representation: 'date' })}`}
+                        to={`/theaters/${theaterId}/day-${index + 1}`}
                         preventScrollReset={true}
                         className={({ isActive, isPending }) => navLinkClassName(isActive, isPending)}
                         key={day.dayNumber}
--- src/router/loaders/MovieScreeningsLoader.tsx
+++ src/router/loaders/MovieScreeningsLoader.tsx
@@ -1,11 +1,9 @@
 import { ActionFunctionArgs } from 'react-router-dom';
 import { getMovieScreeningsByTheaterAndDay as getMovieScreeningsByTheaterIdAndDay } from '../../client/screenings/screenings.client';
-import { formatISO } from 'date-fns';
 
 export const movieScreeningsLoader = async (args: ActionFunctionArgs) => {
     const { params } = args;
-    let { theaterId } = params;
-    const { date } = params;
+    let { theaterId, day } = params;
 
     // Theater id
     if (theaterId === undefined) {
@@ -13,10 +11,12 @@ export const movieScreeningsLoader = async (args: ActionFunctionArgs) => {
         // api call will throw error anyway
     }
 
-    // Selected date
-    let day = formatISO(new Date(), { representation: 'date' });
-    if (date !== undefined) {
-        day = formatISO(new Date(date), { representation: 'date' });
+    // Selected day
+    if (day === undefined) {
+        day = '1';
     }
+    // remove "day"
+    day = day.split('-')[1];
+
     return await getMovieScreeningsByTheaterIdAndDay(theaterId, day);
 };
