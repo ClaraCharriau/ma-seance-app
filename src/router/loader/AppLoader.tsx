cf66d578ee725842633991e130597924b25d0ee9
Clara Charriau <clara.charriau@renault.com>
2024-04-24 15:30:10 +0200

    feat: verify JWT token before login page redirection

--- src/router/loader/AppLoader.tsx
+++ src/router/loader/AppLoader.tsx
@@ -1,13 +1,21 @@
 import { redirect } from 'react-router-dom';
+import { User } from '../../model/User';
+import { jwtDecode } from 'jwt-decode';
 
 /**
- * Loader that check if a logged user exists before the page renders
- * and redirect to login page if the user is not logged
+ * Loader that check if a user token exists and is valid before the page renders.
+ * Redirect to login page if the user is not logged.
  */
 export const appLoader = () => {
     const maSeanceId = localStorage.getItem('maSeanceId');
-    if (!maSeanceId) {
-        return redirect('/login');
+    const parsedToken = maSeanceId ? JSON.parse(maSeanceId) : null;
+
+    if (parsedToken) {
+        const validToken: { user: User } = jwtDecode(parsedToken.access_token);
+
+        if (!validToken.user) {
+            return redirect('/login');
+        }
     }
     return null;
 };
